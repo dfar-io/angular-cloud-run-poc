7a06a07ca0064daa9e48420e99e1453051853a58
Dave Farinelli <d@dfar.io>
2023-01-26 08:22:07 -0500

    Add Cloud SQL as Private IP (#7)

--- tf/sql.tf
+++ tf/sql.tf
@@ -8,12 +8,12 @@ resource "google_sql_database_instance" "instance" {
   settings {
     tier = "db-custom-2-3840"
     ip_configuration {
-      authorized_networks {
-        name  = "CloudRun"
-        value = "${google_compute_address.default.address}/32"
-      }
+      ipv4_enabled    = false
+      private_network = google_compute_network.default.id
     }
   }
+
+  depends_on = [google_service_networking_connection.private_vpc_connection]
 }
 
 resource "random_password" "password" {
@@ -26,4 +26,38 @@ resource "google_sql_user" "users" {
   name     = "app"
   instance = google_sql_database_instance.instance.name
   password = random_password.password.result
+}
+
+# private IP configuration
+resource "google_compute_global_address" "private_ip_address" {
+  project  = local.project_id
+  provider = google-beta
+
+  name          = "cloud-sql-private-ip-address"
+  purpose       = "VPC_PEERING"
+  address_type  = "INTERNAL"
+  prefix_length = 16
+  network       = google_compute_network.default.id
+}
+
+resource "google_service_networking_connection" "private_vpc_connection" {
+  provider = google-beta
+
+  network                 = google_compute_network.default.id
+  service                 = "servicenetworking.googleapis.com"
+  reserved_peering_ranges = [google_compute_global_address.private_ip_address.name]
+
+  depends_on = [google_project_service.service_networking]
+}
+
+resource "google_project_service" "service_networking" {
+  project = local.project_id
+  service = "servicenetworking.googleapis.com"
+
+  timeouts {
+    create = "30m"
+    update = "40m"
+  }
+
+  disable_dependent_services = true
 }
